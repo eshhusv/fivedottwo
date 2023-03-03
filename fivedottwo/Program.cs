0c8a737a39fedd605a447db25f1422ee2840b148
eshhusv <ak7527203@gmail.com>
2023-03-03 20:59:30 +0200

    Add project files.

---
+++ fivedottwo/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr = new int[10];
+Random r = new Random();
+int zeroCounter = 0, oneCounter = 0;
+
+for (int i = 0;i < arr.Length; i++)
+{
+    arr[i] = r.Next(0, 2);
+    if (arr[i] == 0) zeroCounter++;
+    else oneCounter++;
+    Console.Write(arr[i]);
+}
+Console.WriteLine();
+Console.Write($"Кол-во нулей: {zeroCounter}, кол-во единиц: {oneCounter}");
